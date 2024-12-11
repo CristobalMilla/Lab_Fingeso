e315f8dfb11ef368190855dff8f8ed7a98930ada
Ximena Gomez Lucero <114019217+Basryu@users.noreply.github.com>
2024-12-10 22:24:07 -0300

    Creacion y avance permisoService

---
+++ src/main/java/grupo3/LabFingeso/service/permisoService.java
@@ -0,0 +1,15 @@
+package grupo3.LabFingeso.service;
+
+import grupo3.LabFingeso.entity.usuarioEntity;
+import grupo3.LabFingeso.repository.usuarioRepository;
+
+public class permisoService {
+
+    usuarioRepository usuarioRepo;
+
+
+}
+
+
+
+}
