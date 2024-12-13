6f474b5703f2d2dc40e8738de846db944e84a7e8
vicho314 <vicente.carrasco@usach.cl>
2024-12-13 16:51:57 -0300

    pagoController, estandarización nombres de métodos en Service

--- src/main/java/grupo3/LabFingeso/repository/usuarioRepository.java
+++ src/main/java/grupo3/LabFingeso/repository/usuarioRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface usuarioRepository extends JpaRepository<usuarioEntity, Long> {
     usuarioEntity findByCorreo(String email);
-
 }
--- src/main/java/grupo3/LabFingeso/service/pagoService.java
+++ src/main/java/grupo3/LabFingeso/service/pagoService.java
@@ -5,36 +5,44 @@ import grupo3.LabFingeso.repository.pagoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class pagoService {
-    private final pagoRepository pagoRepo;
-
     @Autowired
+    private pagoRepository pagoRepo;
+    
     public pagoService(pagoRepository pagoRepo) {
         this.pagoRepo = pagoRepo;
     }
 
     // Create
-    public pagoEntity save(pagoEntity pago) {
-        return pagoRepo.save(pago);
+    public pagoEntity createPago(pagoEntity pago) {
+        pagoEntity existentePago = pagoRepo.findById(pago.getId()).orElse(null);
+        if(existentePago != null) {
+            return null;
+        }
+        else {
+            return pagoRepo.save(pago);
+        }
     }
 
     // Read
-    public pagoEntity findById(Long id) {
+    public pagoEntity getPagoById(Long id) {
         return pagoRepo.findById(id).orElse(null);
     }
 
-    public Iterable<pagoEntity> findAll() {
+    public List<pagoEntity> getAllPagos() {
         return pagoRepo.findAll();
     }
 
     // Update
-    public pagoEntity update(pagoEntity pago) {
+    public pagoEntity updatePago(pagoEntity pago) {
         return pagoRepo.save(pago);
     }
 
     // Delete
-    public void deleteById(Long id) {
+    public void deletePagoById(Long id) {
         pagoRepo.deleteById(id);
     }
 }
