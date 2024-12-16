0a8694068abdd7129a25dd12a17cd8018aee44dd
Rbd-Antoine <122039637+Rbd-Antoine@users.noreply.github.com>
2024-12-16 02:00:36 -0300

    Footer, sidemenu, busqueda, catalogo y otros paginas

--- FrontEnd/AutoAltiro/src/App.vue
+++ FrontEnd/AutoAltiro/src/App.vue
@@ -1,27 +1,46 @@
-<script setup>
-
-  import { RouterView } from 'vue-router'
-
-</script>
-
-
 <template>
+  <div id="app">
+    <aside class="side-menu">
+      <ul>
+        <li><router-link to="/">Inicio</router-link></li>
+        <li v-if="isAdmin"><router-link to="/admin">Admin</router-link></li>
+        <li><router-link to="/customer">Cliente</router-link></li>
+        <li v-if="isAdmin"><router-link to="/add-car">Añadir un vehículo</router-link></li>
+        <li v-if="isAdmin"><router-link to="/car-status">Estado del vehículo</router-link></li>
 
- <RouterView/>
+        <li><router-link to="/advanced-search">Búsqueda</router-link></li>
+        <li><router-link to="/reserve">Reserva</router-link></li>
+        <li><router-link to="/vehicles">Catalogo de vehículos</router-link></li>
+      </ul>
+    </aside>
 
+    <div class="main-content">
+      <router-view />
+    </div>
+
+    <Footer />
+  </div>
 </template>
 
+<script>
+import Footer from "./components/Footer.vue";
 
-<style>
-html,
-body{
-  height: 100vh;
-  width: 100vw;
-  margin: 0;
-}
-#app {
-  height: inherit;
-  width: inherit;
-}
-
-</style>
+export default {
+  name: "App",
+  components: {
+    Footer,
+  },
+  data() {
+    return {
+      isAdmin: false, 
+    };
+  },
+  created() {
+    // REquest if user is admin to know the state and what to rpompt, will be replaced by a backend get
+    const userRole = localStorage.getItem('userRole'); 
+    if (userRole === 'admin') {
+      this.isAdmin = true; 
+    }
+  },
+};
+</script>
